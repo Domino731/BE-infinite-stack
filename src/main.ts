6a3d683ae8501c046a4362f311c491e192c848d9
Domino731 <d.orzechowski18@gmail.com>
2022-11-17 19:09:44 +0100

    cors fixes

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,15 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-var cors = require('cors');
+const cors = require("cors");
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {cors: true});
+    const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
-    app.enableCors({
-        allowedHeaders: "*",
-        origin: "*"
-    });
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Credentials", true);
-        next();
-    })
+    app.use(cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }));
     await app.listen(8000);
 }
 
