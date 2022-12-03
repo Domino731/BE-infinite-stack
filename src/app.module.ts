5399f6e48284cd30a378660c48988760371957ad
Domino731 <d.orzechowski18@gmail.com>
2022-12-03 15:53:09 +0100

    connect to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './api/users/user.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './api/users/user.module';
 import config from './config';
-import { MongooseModule } from '@nestjs/mongoose';
+import {MongooseModule} from '@nestjs/mongoose';
+import {config as ORM_CONFIG} from './orm.config'
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [MongooseModule.forRoot(config.MONGO), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot(config.MONGO), UserModule, TypeOrmModule.forRoot(ORM_CONFIG)],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/orm.config.ts
@@ -0,0 +1,15 @@
+import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+import APP_CONFIG from "./config";
+
+export const config: TypeOrmModuleOptions = {
+    type: 'postgres',
+    port: 5432,
+    username: 'postgres',
+    password: APP_CONFIG.POSTGRES_PASSWORD,
+    host: '172.0.0.1',
+    database: 'Infinite_stack',
+    // creating new key in table automatically, for example if you have table called companies(id, name), you can easily add year key
+    synchronize: true,
+    entities: ['dist/**/*.entity{.js, .ts}']
+
+}
